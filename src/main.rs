2adcb8e467e2f76b5ab83721990c14999c0508cf
Zach Nielsen <zach@zachn.me>
2021-03-31 20:38:34 -0700

    Add range to query. Combine ranges

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ type Json = HashMap<String, Variant>;
 
 pub const ENDPOINT: &'static str = "https://na1.api.riotgames.com";
 
+// Interested in:
+// Our teams role names -> get summoner name for each role
+// Result of the game (win/loss/none)
+// KDA of each person?
+
+
 /// Function expects API key to be the only thing in the file
 fn get_api_key() -> Result<String, std::io::Error> {
     let key = std::fs::read_to_string("api.key")?.replace("\n", "");
@@ -24,10 +30,11 @@ fn get_encrypted_account_id(summ_name: &str) -> Result<String, Box<dyn std::erro
 }
 
 fn get_matches(id: &str, start_idx: i32, end_idx: i32) -> Result<Json, Box<dyn std::error::Error>> {
-    // TODO put range into request
     let api_endpoint_base = String::from("/lol/match/v4/matchlists/by-account");
     let slug = api_endpoint_base + "/" + id +
-        "?api_key=" + &get_api_key()?;
+        "?api_key=" + &get_api_key()? +
+        "?beginIndex=" + &start_idx.to_string() +
+        "?endIndex=" + &end_idx.to_string();
     let request = String::from(ENDPOINT) + &slug;
     println!("Sending reqwest: {}", request);
     let resp = reqwest::blocking::get(request)?;
@@ -55,46 +62,49 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("encrypted account id : {}" , enc_account_id);
 
     let mut more_matches = true;
+    // Limit of 100 matches at a time. Grab 100 at a time until there are no more
+    let mut start_idx = 0;
+    let mut end_idx = 100;
     while more_matches {
-        // get match history by encrypted account Id
-            // Limit of 100 matches at a time. Grab 100 at a time until there are no more
-        let mut start_idx = 0;
-        let mut end_idx = 99;
         let matches = get_matches(&enc_account_id, start_idx, end_idx)?;
         println!("matches: {:?}", matches);
+
+        // Set up the next indexes
+        start_idx = end_idx+1;
+        end_idx = start_idx + 100;
+
+        let range_start: i64 = matches["startIdx"].clone().into();
+        let range_end: i64 = matches["endIdx"].clone().into();
+        more_matches = range_end-range_start == 100;
+
         let games: Vec<Json> = matches["matches"].clone().into();
         for game in games {
             let game_id: i64 = game["gameId"].clone().into();
             let game_info = get_game_info(&game_id.to_string())?;
 
-            // TODO - zip all these together and iterate over them at once
-
             let participant_identities: Vec<Json> = game_info["participantIdentities"].clone().into();
-            for participant_identity in participant_identities {
+            let stats: Vec<Json> = game_info["teams"].clone().into();
+            let participants: Vec<Json> = game_info["participants"].clone().into();
+            let iter = participant_identities.iter()
+                .zip(participants.iter())
+                .zip(stats.iter())
+                .map(|((x, y), z)| (x, y, z));
+
+            for it in iter {
+                let (participant_identity, participant, stat) = it;
+
                 let participant_id: i64 = participant_identity["participantId"].clone().into();
                 let player: Json = participant_identity["player"].clone().into();
                 let summoner_name: String = player["summonerName"].clone().into();
                 let summoner_id: String = player["summonerId"].clone().into();
-            }
 
-            let stats: Vec<Json> = game_info["teams"].clone().into();
-            for stat in stats {
                 let win: String = stat["win"].clone().into();
-            }
 
-            let participants: Vec<Json> = game_info["participants"].clone().into();
-            for participant in participants {
                 let team_id: i64 = participant["teamId"].clone().into();
                 let participant_id: i64 = participant["participantId"].clone().into();
             }
         }
-        more_matches = false;
     }
-
-    // Interested in:
-    // Our teams role names -> get summoner name for each role
-    // Result of the game (win/loss/none)
-    // KDA of each person?
 
     println!("end of main\n");
     Ok(())
