d537ec19d1265f48d14369497660d7345e25be8e
Zach Nielsen <zach@zachn.me>
2021-04-03 13:51:07 -0700

    Remove variant, add async and internal data representation (way cleaner once it was done, should have just done it in the first place)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 use std::vec::Vec;
 
-// type Json = HashMap<String, Variant>;
-type Json = HashMap<String, serde_json::Value>;
+use serde_json::Value;
+use reqwest::Client;
+use serde::Deserialize;
+
+type Json = HashMap<String, Value>;
 
 pub const ENDPOINT: &'static str = "https://na1.api.riotgames.com";
 pub const BLUE_SIDE: i64 = 100;
@@ -14,164 +17,193 @@ pub const RED_SIDE: i64 = 200;
 // KDA of each person?
 
 // My Data
-enum GameResult {
+#[derive(Debug)]
+enum GameResultData {
     Win,
     Loss,
     Other
 }
-struct Player {
+#[derive(Debug)]
+struct PlayerData {
     lane: String,
     summ_name: String,
     summ_id: String,
 }
-struct Game {
-    result: GameResult,
-    team: Vec<Player>,
+#[derive(Debug)]
+struct GameData {
+    result: GameResultData,
+    team: Vec<PlayerData>,
     team_of_interest: i64,
 }
 
 // Rito data for serde_json
-// struct JsonGame {
-//     gameId: i64
-// }
-
-//             let stats: Vec<Json> = game_info["teams"];
-//             let participant_identities: Vec<Json> = game_info["participantIdentities"];
-//             let participants: Vec<Json> = game_info["participants"];
-//                 let participant_id_id: i64 = participant_identity["participantId"];
-//                 let player: Json = participant_identity["player"];
-//                 let summoner_name: String = player["summonerName"];
-//                 let summoner_id: String = player["summonerId"];
-
-//                 let team_id: i64 = participant["teamId"];
-//                 let participant_id: i64 = participant["participantId"];
-//                 let timeline: Json = participant["timeline"];
-//                 let lane: String = timeline["lane"];
-
-/// Function expects API key to be the only thing in the file
-fn get_api_key() -> Result<String, std::io::Error> {
-    let key = std::fs::read_to_string("api.key")?.replace("\n", "");
-    println!("key: {}", key);
-    return Ok(key);
+#[derive(Debug, Deserialize)]
+struct GameInfo {
+    gameId: i64,
+    teams: Vec<Win>,
+    participantIdentities: Vec<ParticipantId>,
+    participants: Vec<Participant>,
 }
 
-fn get_encrypted_account_id(summ_name: &str) -> Result<String, Box<dyn std::error::Error>> {
+#[derive(Debug, Deserialize)]
+struct Player {
+    summonerName: String,
+    summonerId: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Timeline {
+    lane: String
+}
+
+#[derive(Debug, Deserialize)]
+struct Participant {
+    teamId: i64,
+    participantId: i64,
+    timeline: Timeline
+}
+
+#[derive(Debug, Deserialize)]
+struct ParticipantId {
+    participantId: i64,
+    player: Player,
+}
+
+#[derive(Debug, Deserialize)]
+struct Win {
+    win: String
+}
+
+#[derive(Debug, Deserialize)]
+struct Match {
+    gameId: i64
+}
+
+#[derive(Debug, Deserialize)]
+struct Matches {
+    startIndex: i64,
+    endIndex: i64,
+    matches: Vec<Match>,
+}
+
+/// Function expects API key to be the only thing in the file
+/// Only read once, then store the string statically
+fn get_api_key() -> Result<String, std::io::Error> {
+    static mut key: String = String::from("");
+    unsafe {
+        if key == "" {
+            key = std::fs::read_to_string("api.key")?.replace("\n", "");
+            println!("key: {}", key);
+        }
+    }
+    return Ok(key.clone());
+}
+
+async fn get_encrypted_account_id(summ_name: &str) -> Result<String, Box<dyn std::error::Error>> {
     let slug = String::from("/lol/summoner/v4/summoners/by-name/") + summ_name +
         "?api_key=" + &get_api_key()?;
     let request = String::from(ENDPOINT) + &slug;
     println!("Sending reqwest: {}", request);
-    let resp = reqwest::blocking::get(request)?;
-    let j = resp.json::<serde_json::Value>()?;
+    let resp = reqwest::get(request).await?;
+    let j = resp.json::<Value>().await?;
     return Ok(j["accountId"].to_string());
 }
 
-fn get_matches(id: &str, start_idx: i32, end_idx: i32) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-    let api_endpoint_base = String::from("/lol/match/v4/matchlists/by-account");
+async fn get_matches(client: &Client, id: &str, start_idx: i64, end_idx: i64) -> Result<Matches, Box<dyn std::error::Error>> {
+    let api_endpoint_base = String::from("/lol/match/v4/watchlists/by-account");
     let slug = api_endpoint_base + "/" + id +
         "?endIndex=" + &end_idx.to_string() +
         "&beginIndex=" + &start_idx.to_string() +
         "&api_key=" + &get_api_key()?;
     let request = String::from(ENDPOINT) + &slug;
     println!("Sending reqwest: {}", request);
-    let resp = reqwest::blocking::get(&request)?;
-    let j = resp.json::<serde_json::Value>()?;
+    let resp = client.get(&request).send().await?;
+    let j = resp.json::<Matches>().await?;
     return Ok(j);
 }
 
-fn get_game_info(game_id: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+async fn get_game_info(client: &Client, game_id: &str) -> Result<GameInfo, Box<dyn std::error::Error>> {
     let api_endpoint_base = String::from("/lol/match/v4/matches/");
     let slug = api_endpoint_base + game_id +
         "?api_key=" + &get_api_key()?;
     let request = String::from(ENDPOINT) + &slug;
     println!("Sending reqwest: {}", request);
-    let resp = reqwest::blocking::get(request)?;
-    let j = resp.json::<serde_json::Value>()?;
+    let resp = client.get(request).send().await?;
+    let j = resp.json::<GameInfo>().await?;
     return Ok(j);
 
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut data = Vec::<Game>::new();
+async fn collect_data(summoner: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut data = Vec::<GameData>::new();
     let summoner = String::from("Suq Mediq");
 
-    let enc_account_id = get_encrypted_account_id(&summoner)?;
+    let enc_account_id = get_encrypted_account_id(&summoner).await?;
     println!("encrypted account id : {}" , enc_account_id);
 
+    // Create a reqwest Client
+    let client = reqwest::Client::new();
+
     let mut more_matches = true;
-    // Limit of 100 matches at a time. Grab 100 at a time until there are no more
-    let mut start_idx = 0;
-    let mut end_idx = 100;
+    // The API has a limit of 100 matches at a time. Grab 100 at a time until there are no more
+    let mut start_idx: i64 = 0;
+    let mut end_idx: i64 = 100;
     while more_matches {
-        let matches = get_matches(&enc_account_id, start_idx, end_idx)?;
+        let matches = get_matches(&client, &enc_account_id, start_idx, end_idx).await?;
         println!("matches: {:?}", matches);
 
         // Set up the next indexes
         start_idx = end_idx+1;
         end_idx = start_idx + 100;
 
-        let range_start: i64 = matches["startIndex"].as_i64()?;
-        let range_end: i64 = matches["endIndex"].as_i64()?;
+        let range_start = matches.startIndex;
+        let range_end = matches.endIndex;
         more_matches = range_end-range_start == 100;
 
-        let games: Vec<serde_json::Value> = matches["matches"];
-        for a_game in games {
-            let mut game = Game {
-                result: GameResult::Other,
+        // TODO - Split this up into multiple threads
+        // https://stackoverflow.com/questions/51044467/how-can-i-perform-parallel-asynchronous-http-get-requests-with-reqwest
+        for a_match in matches.matches {
+            let mut game = GameData {
+                result: GameResultData::Other,
                 team: Vec::new(),
                 team_of_interest: 0,
             };
 
-            let game_id: i64 = a_game["gameId"];
-            let game_info = get_game_info(&game_id.to_string())?;
+            let game_id = a_match.gameId;
+            let game_info = get_game_info(&client, &game_id.to_string()).await?;
 
-            let stats: Vec<Json> = game_info["teams"];
-            let participant_identities: Vec<Json> = game_info["participantIdentities"];
-            let participants: Vec<Json> = game_info["participants"];
-            assert!(participant_identities.len() == participants.len());
-            let iter = participant_identities.iter()
-                .zip(participants.iter())
+            assert!(game_info.participantIdentities.len() == game_info.participants.len());
+            let iter = game_info.participantIdentities.iter()
+                .zip(game_info.participants.iter())
                 .map(|(x, y)| (x, y));
 
-
             // Get all the participants for this game
-            let mut blue_team: Vec<Player> = Vec::new();
-            let mut red_team: Vec<Player> = Vec::new();
+            let mut blue_team: Vec<PlayerData> = Vec::new();
+            let mut red_team: Vec<PlayerData> = Vec::new();
             for it in iter {
                 let (participant_identity, participant) = it;
 
-                let participant_id_id: i64 = participant_identity["participantId"];
-                let player: Json = participant_identity["player"];
-                let summoner_name: String = player["summonerName"];
-                let summoner_id: String = player["summonerId"];
+                assert!(participant.participantId == participant_identity.participantId);
 
-                let team_id: i64 = participant["teamId"];
-                let participant_id: i64 = participant["participantId"];
-                let timeline: Json = participant["timeline"];
-                let lane: String = timeline["lane"];
-
-                assert!(participant_id_id == participant_id);
-
-                if summoner_name == summoner {
-                    game.team_of_interest = team_id;
+                if participant_identity.player.summonerName == summoner {
+                    game.team_of_interest = participant.teamId;
                 }
 
-                let p = Player {
-                    lane: lane,
-                    summ_name: summoner_name,
-                    summ_id: summoner_id,
+                let p = PlayerData {
+                    lane: participant.timeline.lane.clone(),
+                    summ_name: participant_identity.player.summonerName.clone(),
+                    summ_id: participant_identity.player.summonerId.clone(),
 
                 };
 
-                match team_id {
+                match participant.teamId {
                     BLUE_SIDE => blue_team.push(p),
                     RED_SIDE => red_team.push(p),
-                    _ => panic!("Got a team id of {}", team_id),
+                    _ => panic!("Got a team id of {}", participant.teamId),
                 }
             }
 
-            for stat in stats {
-                let win: String = stat["win"];
-            }
+            // TODO - track win here
 
             match game.team_of_interest {
                 BLUE_SIDE => game.team = blue_team,
@@ -183,5 +215,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("end of main\n");
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
