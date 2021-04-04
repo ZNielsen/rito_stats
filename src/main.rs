a0df27632df11e342975ba0afdc2a34329cca521
Zach Nielsen <zach@zachn.me>
2021-04-03 18:07:23 -0700

    Cleaning up typos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use std::collections::HashMap;
+#![allow(non_snake_case)]
 use std::vec::Vec;
-
 use serde_json::Value;
 use reqwest::Client;
 use serde::Deserialize;
-
-type Json = HashMap<String, Value>;
 
 pub const ENDPOINT: &'static str = "https://na1.api.riotgames.com";
 pub const BLUE_SIDE: i64 = 100;
@@ -20,7 +17,6 @@ pub const RED_SIDE: i64 = 200;
 #[derive(Debug)]
 enum GameResultData {
     Win,
-    Loss,
     Other
 }
 #[derive(Debug)]
@@ -37,14 +33,6 @@ struct GameData {
 }
 
 // Rito data for serde_json
-#[derive(Debug, Deserialize)]
-struct GameInfo {
-    gameId: i64,
-    teams: Vec<Win>,
-    participantIdentities: Vec<ParticipantId>,
-    participants: Vec<Participant>,
-}
-
 #[derive(Debug, Deserialize)]
 struct Player {
     summonerName: String,
@@ -70,8 +58,17 @@ struct ParticipantId {
 }
 
 #[derive(Debug, Deserialize)]
-struct Win {
-    win: String
+struct TeamStats {
+    win: String,
+    teamId: i64,
+}
+
+#[derive(Debug, Deserialize)]
+struct GameInfo {
+    gameId: i64,
+    teams: Vec<TeamStats>,
+    participantIdentities: Vec<ParticipantId>,
+    participants: Vec<Participant>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -100,11 +97,12 @@ async fn get_encrypted_account_id(summ_name: &str) -> Result<String, Box<dyn std
     println!("Sending reqwest: {}", request);
     let resp = reqwest::get(request).await?;
     let j = resp.json::<Value>().await?;
-    return Ok(j["accountId"].to_string());
+    let ret = j["accountId"].to_string().replace("\"", "");
+    return Ok(ret);
 }
 
 async fn get_matches(client: &Client, id: &str, start_idx: i64, end_idx: i64) -> Result<Matches, Box<dyn std::error::Error>> {
-    let api_endpoint_base = String::from("/lol/match/v4/watchlists/by-account");
+    let api_endpoint_base = String::from("/lol/match/v4/matchlists/by-account");
     let slug = api_endpoint_base + "/" + id +
         "?endIndex=" + &end_idx.to_string() +
         "&beginIndex=" + &start_idx.to_string() +
@@ -112,8 +110,8 @@ async fn get_matches(client: &Client, id: &str, start_idx: i64, end_idx: i64) ->
     let request = String::from(ENDPOINT) + &slug;
     println!("Sending reqwest: {}", request);
     let resp = client.get(&request).send().await?;
-    let j = resp.json::<Matches>().await?;
-    return Ok(j);
+
+    Ok(resp.json::<Matches>().await?)
 }
 
 async fn get_game_info(client: &Client, game_id: &str) -> Result<GameInfo, Box<dyn std::error::Error>> {
@@ -123,14 +121,12 @@ async fn get_game_info(client: &Client, game_id: &str) -> Result<GameInfo, Box<d
     let request = String::from(ENDPOINT) + &slug;
     println!("Sending reqwest: {}", request);
     let resp = client.get(request).send().await?;
-    let j = resp.json::<GameInfo>().await?;
-    return Ok(j);
 
+    Ok(resp.json::<GameInfo>().await?)
 }
 
-async fn collect_data(summoner: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn collect_data(summoner: &str) -> Result<Vec<GameData>, Box<dyn std::error::Error>> {
     let mut data = Vec::<GameData>::new();
-    let summoner = String::from("Suq Mediq");
 
     let enc_account_id = get_encrypted_account_id(&summoner).await?;
     println!("encrypted account id : {}" , enc_account_id);
@@ -197,7 +193,13 @@ async fn collect_data(summoner: &str) -> Result<(), Box<dyn std::error::Error>> 
                 }
             }
 
-            // TODO - track win here
+            for team_stats in game_info.teams {
+                if team_stats.teamId == game.team_of_interest {
+                    if team_stats.win == "Win" {
+                        game.result = GameResultData::Win;
+                    }
+                }
+            }
 
             match game.team_of_interest {
                 BLUE_SIDE => game.team = blue_team,
@@ -209,10 +211,16 @@ async fn collect_data(summoner: &str) -> Result<(), Box<dyn std::error::Error>> 
     }
 
     println!("end of main\n");
-    Ok(())
+    Ok(data)
+}
+
+fn analyze_data(_data: &Vec<GameData>) {
+    println!("Data will be analyzed here");
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data = collect_data("Suq Mediq").await?;
+    analyze_data(&data);
     Ok(())
 }
